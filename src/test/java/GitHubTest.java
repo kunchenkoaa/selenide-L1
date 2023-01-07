d053a6643baeda772f8292c3de61099e19289e73
Compudaster <gerwurd@yandex.ru>
2023-01-07 22:13:07 +0300

    add test for SoftAssertions and JUnit5

---
+++ src/test/java/GitHubTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubTest {
+    @Test
+    void successfulSearchTest() {
+        open("https://github.com/selenide/selenide");
+        $("[id=wiki-tab]").click();
+        $(byText("Show 2 more pages…")).click();
+        $("div").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("div").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
