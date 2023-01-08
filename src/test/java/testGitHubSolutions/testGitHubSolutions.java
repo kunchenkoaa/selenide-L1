26dcf6c0a07b7be089e5dde0930d400addd7ac53
Compudaster <gerwurd@yandex.ru>
2023-01-08 17:24:19 +0300

    add test for Solutions -> Enterprise

---
+++ src/test/java/testGitHubSolutions/testGitHubSolutions.java
@@ -0,0 +1,18 @@
+package testGitHubSolutions;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class testGitHubSolutions {
+    @Test
+    void testSolution() {
+        open("https://github.com");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("div.col-9-max").shouldHave(text("Build like the best"));
+                sleep (1500);
+    }
+}
