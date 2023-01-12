2e2420762403dd52280ce7eafc1bb024c54fad89
Compudaster <gerwurd@yandex.ru>
2023-01-12 22:56:08 +0300

    add verify for element B

--- src/test/java/testDragAndDrop/testDragAndDrop.java
+++ src/test/java/testDragAndDrop/testDragAndDrop.java
@@ -11,6 +11,6 @@ public class testDragAndDrop {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("[id=column-a]").dragAndDropTo($("[id=column-b]"));
         $("[id=column-a]").shouldHave(text("B"));
-
+        $("[id=column-b]").shouldHave(text("A"));
     }
 }
